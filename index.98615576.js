d3f950b51345712f94db39df7514fbd880a6b32e
Jordan Bourbonnais <clonage@gmail.com>
2022-12-09 04:51:14 +0000

    xxx

--- index.98615576.js
+++ index.98615576.js
@@ -1,4 +1,8 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Open new tab<br>Count ${r}`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
+(function(){const r=document.createElement("link").relList;
+if(r&&r.supports&&r.supports("modulepreload"))return;
+for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};
+return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);
+  fetch(e.href,t)}})();function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Open new tab<br>Count ${r}`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
     <a href="about.html" target="">
       <img src="/vite.svg" class="logo" alt="ZipppIT" />
@@ -13,7 +17,7 @@
     </div>
     <div class="card">
     <a href="https://bit.ly/shaffaiphone2" target="_blank">
-    <button id="counter" type="button"></button>
+    <button id="counter" type="button">xxx</button>
   </a>
      
     </div>
